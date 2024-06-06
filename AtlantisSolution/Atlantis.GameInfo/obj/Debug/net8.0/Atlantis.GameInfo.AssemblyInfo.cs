cb2f62ff35a5d62d643ca29eeae99813facf40d3
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2024-06-05 22:34:32 -0300

    up

--- AtlantisSolution/Atlantis.GameInfo/obj/Debug/net8.0/Atlantis.GameInfo.AssemblyInfo.cs
+++ AtlantisSolution/Atlantis.GameInfo/obj/Debug/net8.0/Atlantis.GameInfo.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Atlantis.Data")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Atlantis.GameInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83261bccbaf486ac35387ecbb09bb07e43019ac")]
-[assembly: System.Reflection.AssemblyProductAttribute("Atlantis.Data")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Atlantis.Data")]
+[assembly: System.Reflection.AssemblyProductAttribute("Atlantis.GameInfo")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Atlantis.GameInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
--- FuseNetwork/Fuse.Service/obj/Debug/net8.0/Fuse.Service.AssemblyInfo.cs
+++ FuseNetwork/Fuse.Service/obj/Debug/net8.0/Fuse.Service.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Atlantis.Data")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Fuse.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83261bccbaf486ac35387ecbb09bb07e43019ac")]
-[assembly: System.Reflection.AssemblyProductAttribute("Atlantis.Data")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Atlantis.Data")]
+[assembly: System.Reflection.AssemblyProductAttribute("Fuse.Service")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Fuse.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
